0e1dcc6415deb685bd7354624aa104bdcab2035e
Samir Soormally <samirsoormally@gmail.com>
2018-10-09 12:08:20 +0100

    refactor: adds subjects to tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,28 +1,26 @@
 require "./lib/transaction.rb"
 
 describe "transaction" do
+  subject(:deposit_transaction) { Transaction.new(200, "", mock_account.balance) }
+  subject(:withdrawal_transaction) { Transaction.new("", 100, mock_account.balance) }
   let(:mock_account) { double('account', balance: 100) }
 
   describe "#new" do
     context "when a transaction has been made" do
-      before(:each) do
-        @deposit_transaction = Transaction.new(200, "", mock_account.balance)
-        @withdrawal_transaction = Transaction.new("", 100, mock_account.balance)
-      end
       describe "state" do
         it "knows the date" do
-          expect(@deposit_transaction.date).to eq(time_now)
+          expect(deposit_transaction.date).to eq(time_now)
         end
         it "knows how much is creditted" do
-          expect(@deposit_transaction.credit).to eq(200)
-          expect(@withdrawal_transaction.credit).to eq("")
+          expect(deposit_transaction.credit).to eq(200)
+          expect(withdrawal_transaction.credit).to eq("")
         end
         it "knows how much is debitted" do
-          expect(@deposit_transaction.debit).to eq("")
-          expect(@withdrawal_transaction.debit).to eq(100)
+          expect(deposit_transaction.debit).to eq("")
+          expect(withdrawal_transaction.debit).to eq(100)
         end
         it "knows the remaining account balance" do
-          expect(@deposit_transaction.balance).to eq(100)
+          expect(deposit_transaction.balance).to eq(100)
         end
       end
     end
